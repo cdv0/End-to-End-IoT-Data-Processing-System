b4f77bac4f6ba73feca7b43292f3c18bfd3f2502
cdv0 <155689506+cdv0@users.noreply.github.com>
2024-11-24 18:35:40 -0800

    Add logic for query_three()

--- tcp_server.py
+++ tcp_server.py
@@ -92,6 +92,51 @@ def query_two(db_connection_meta, db_connection_virtual):
     return f"{average_waterconsumption:.4f}"
 
 
+def query_three(db_connection_meta, db_connection_virtual):
+    # Query 3: Which device consumed more electricity among my three IoT devices (two refrigerators and a dishwasher)?
+
+    # Get all device uid
+    device1_fridge_uid = get_device_uid(db_connection_meta, "Device 1: Smart Refrigerator")
+    device2_dishwasher_uid = get_device_uid(db_connection_meta, "Device 2: Smart Dishwasher")
+    device3_fridge_uid = get_device_uid(db_connection_meta, "Device 3: Smart Refrigerator")
+
+    # Get a list of all data
+    documents = db_connection_virtual.find({})
+
+    # Total the ammeter values for each device
+    device1_total = 0
+    device2_total = 0
+    device3_total = 0
+
+    for doc in documents:
+        doc_uid = doc.get("payload", {}).get("parent_asset_uid")
+        if doc_uid is not None:
+            ammeter1 = float(doc.get("payload", {}).get("Ammeter", 0.0))
+            ammeter2 = float(doc.get("payload", {}).get("Ammeter 2", 0.0))
+            ammeter3 = float(doc.get("payload", {}).get("Ammeter 3", 0.0))
+
+            # Only sum up positive ammeter values, where a positive value indicates it's using electricity
+            # A negative ammeter value means it's returning electricity, so ignore negative values
+            if (device1_fridge_uid == doc_uid) and (ammeter1 > 0):
+                device1_total += ammeter1
+            elif (device2_dishwasher_uid == doc_uid) and (ammeter2 > 0):
+                device2_total += ammeter2
+            elif (device3_fridge_uid == doc_uid) and (ammeter3 > 0):
+                device3_total += ammeter3
+    
+    # Determine which device used the most electricity
+    max_value = max(device1_total, device2_total, device3_total)
+
+    if max_value == device1_total:
+        max_device = "Device 1: Smart Refrigerator"
+    elif max_value == device2_total:
+        max_device = "Device 2: Smart Dishwasher"
+    else:
+        max_device = "Device 3: Smart Refrigerator"
+    
+    return f"Device that used the most electricity: {max_device} with {max_value:.4f} amperes (A)."
+
+
 def run_server():
     # Input the port number and IP address
     ipaddress = str(ipaddress.ip_address(input("Input the IP address: ")))
